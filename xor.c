ed16456996941a3b001dfa75032a5efe23d97a1c
Sad837 <sio1452@g.rit.edu>
2026-09-22 22:45:55 -0400

    length corrections

--- xor.c
+++ xor.c
@@ -16,9 +16,9 @@ void encryptFile(const char *filename, const char *message, const char *key) {
         printf("Error opening file for writing.\n");
         return;
     }
-
+	size_t len = strlen(message); // save length 
     xorEncrypt((char *)message, key);
-    fwrite(message, 1, strlen(message), file);
+    fwrite(message, 1, len, file); //uses saved length 
 
     fclose(file);
 }
